b22de68eae1820cf77c81e7a5818e34ce5897fd6
Imesh29 <ridrajakaruna29@gmail.com>
2025-12-24 01:33:28 +0530

    Creating Task Form Component

---
+++ src/components/TaskForm.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+export const TaskForm = () => {
+  return (
+    <header className="app_header">
+      <form>
+        <input
+          type="text"
+          className="task_input"
+          placeholder="Enteer Your task"
+        />
+
+        <div className="task_form_bottom_line">
+          <button className="tag">HTML</button>
+          <button className="tag">CSS</button>
+          <button className="tag">Javascript</button>
+          <button className="tag">React</button>
+
+          <select className="task_status">
+            <option value="todo">To Do</option>
+            <option value="doing">Doing</option>
+            <option value="done">Done</option>
+          </select>
+          <button type="submit" className="task_submit">
+            + Add Task
+          </button>
+        </div>
+      </form>
+    </header>
+  );
+};
+
+export default TaskForm;
