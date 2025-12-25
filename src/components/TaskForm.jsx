0c215cc6b8e32f63203150fdae75d108a800f7e7
Imesh29 <ridrajakaruna29@gmail.com>
2025-12-25 23:50:04 +0530

    Display selected Tag on UI

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -1,28 +1,101 @@
-import React from "react";
+import React, { useState } from "react";
 import "./TaskForm.css";
 import Tag from "./Tag";
 
-export const TaskForm = () => {
+const TaskForm = ({ setTasks }) => {
+  //Create single state variable
+  const [taskData, setTaskData] = useState({
+    task: "",
+    status: "todo",
+    tags: [],
+  });
+
+  const checkTag = (tag) => {
+    // Use to check the tag is already in the array or not
+    return taskData.tags.some((item) => item === tag); // Some methord return true or false value
+  };
+
+  const selectTag = (tag) => {
+    if (taskData.tags.some((item) => item === tag)) {
+      // Use to check the tag is already in the array or not
+      const filterTags = taskData.tags.filter((item) => item !== tag); // Remove the item from the array
+      setTaskData((prev) => {
+        //Update data by new filterTags
+        return { ...prev, tags: filterTags };
+      });
+    } else {
+      setTaskData((prev) => {
+        return { ...prev, tags: [...prev.tags, tag] };
+      });
+    }
+  };
+
+  const handleChange = (e) => {
+    //For all form data
+    const { name, value } = e.target;
+
+    setTaskData((prev) => {
+      return { ...prev, [name]: value };
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault(); //This function prevent from default behaviour of refresh page
+    console.log(taskData);
+    setTasks((prev) => {
+      return [...prev, taskData];
+    });
+    setTaskData({
+      task: "",
+      status: "todo",
+      tags: [],
+    });
+  };
+
   return (
     <header className="app_header">
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
           type="text"
+          name="task" //Use for getting the form field data
+          value={taskData.task}
           className="task_input"
-          placeholder="Enter Your task"
+          placeholder="Enter your task"
+          onChange={handleChange}
         />
 
         <div className="task_form_bottom_line">
           <div>
-            <Tag tagName="HTML" />
-            <Tag tagName="CSS" />
-            <Tag tagName="Javascript" />
-            <Tag tagName="React" />
+            <Tag
+              tagName="HTML"
+              selectTag={selectTag}
+              selected={checkTag("HTML")}
+            />
+            <Tag
+              tagName="CSS"
+              selectTag={selectTag}
+              selected={checkTag("CSS")}
+            />
+            <Tag
+              tagName="JavaScript"
+              selectTag={selectTag}
+              selected={checkTag("JavaScript")}
+            />
+            <Tag
+              tagName="React"
+              selectTag={selectTag}
+              selected={checkTag("React")}
+            />
           </div>
 
           <div>
-            <select className="task_status">
-              <option value="todo">To Do</option>
+            <select
+              name="status"
+              value={taskData.status}
+              className="task_status"
+              onChange={handleChange}
+            >
+              <option value="todo">To do</option>
               <option value="doing">Doing</option>
               <option value="done">Done</option>
             </select>
